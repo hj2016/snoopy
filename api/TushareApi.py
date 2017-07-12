2d5a343412410e8500a8bfdc18475cbafaf9b49c
hj2016 <hj3245459@163.com>
2017-07-13 00:12:53 +0800

    api 添加

--- api/TushareApi.py
+++ api/TushareApi.py
@@ -1,24 +1,39 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'huang jing'
 import tushare as ts
+import sys
 from datetime import datetime
+
+
 class TushareApi:
-    path="/home/huangjing/data/"
+    path = "/home/huangjing/data/"
 
     @staticmethod
-    def get_hist_data(self,start=None, end=None,
-                  ktype='D', retry_count=3,
-                  pause=0.001):
+    def get_hist_data(self, start=None, end=None):
 
         stocks = ts.get_stock_basics()
         for stock in stocks.index:
-            stockInfo = ts.get_h_data(code=stock,start=start,end=end)
-            if(not stockInfo is None):
-                dataPath=self.path+"hist_data/"+str(stock)+".csv"
-                stockInfo.to_csv(dataPath,header=False)
+            stockInfo = ts.get_h_data(code=stock, start=start, end=end)
+            if (not stockInfo is None):
+                dataPath = TushareApi.path + "hist_data/" + str(stock) + ".csv"
+                stockInfo.to_csv(dataPath, header=False)
 
+    @staticmethod
+    def get_industry_classified():
+        df = ts.get_industry_classified()
+        dataPath = TushareApi.path + "industry_classified/" + "industry_classified.csv"
+        df.to_csv(dataPath, header=False)
 
-
+    @staticmethod
+    def get_concept_classified():
+        df = ts.get_concept_classified()
+        dataPath = TushareApi.path + "concept_classified/" + "concept_classified.csv"
+        df.to_csv(dataPath, header=False)
+    @staticmethod
+    def get_stock_basics():
+        df = ts.get_stock_basics()
+        dataPath = TushareApi.path + "stock_basics/" + "stock_basics.csv"
+        df.to_csv(dataPath, header=False)
 if __name__ == '__main__':
     """
     a=datetime.now()
@@ -28,5 +43,12 @@ if __name__ == '__main__':
     print (b-a).microseconds
     df=ts.get_h_data(600848)
     print df.shape[0]
-    """
     TushareApi.get_hist_data('2017-07-07','2017-07-08')
+    TushareApi.get_industry_classified()
+    TushareApi.get_concept_classified()
+    """
+
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    TushareApi.get_stock_basics()
+    ts.get_report_data()
